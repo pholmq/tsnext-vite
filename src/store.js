fe1d272abc56557eed17fd4196b527a66146e9cd
phqrew <patrik.holmqvist@qrew.se>
2023-03-24 19:03:13 +0100

    AnimationController done

---
+++ src/store.js
@@ -0,0 +1,15 @@
+import { createRef } from "react";
+import create from "zustand";
+import { getDefaultSpeedFact, posToDate, posToTime } from "./utils/time-date-functions";
+
+export const useStore = create((set) => ({
+    posRef: createRef(),
+    date: posToDate(0),
+    time: posToTime(0),
+    run: false,
+    speedFact: getDefaultSpeedFact(),
+    orbits: true,
+    orbitsLinewidth: 1,
+    arrows: false,
+    arrowScale: 1
+}));
