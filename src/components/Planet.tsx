d83298358708c4b188186156674d66fe7a215b22
themangokid <hermannylen@gmail.com>
2024-10-01 23:55:49 +0200

    Added rings to planets! Hurray! They have the same texture for now. Perhaps this needs to be moved to another place I don't know.

--- src/components/Planet.tsx
+++ src/components/Planet.tsx
@@ -6,16 +6,11 @@ import { CelestialSphere } from "./CelestialSphere";
 import { useFrame } from "@react-three/fiber";
 import PlanetCamera from "./PlanetCamera";
 import { ContextMenu } from "./ContextMenu";
-
-function PlanetTexture({ texture }) {
-  const [planetTexture] = useTexture([texture]);
-  return <meshStandardMaterial map={planetTexture} />;
-}
+import { PlanetRings } from "./PlanetRings"; // Import the PlanetRings component
 
 export function Planet(props: any) {
   const ref: any = useRef();
   const posRef: any = useStore((state) => state.posRef);
-  // const [planetTexture] = useTexture([props.texture]);
 
   const [hovered, setHover] = useState(false);
   const [contextMenu, setContextMenu] = useState(false);
@@ -30,13 +25,22 @@ export function Planet(props: any) {
 
   const rotationSpeed = props.rotationSpeed || 0;
   const rotationStart = props.rotationStart || 0;
+
   useFrame(() => {
     ref.current.rotation.y = rotationStart + rotationSpeed * posRef.current;
-    // cloudsRef.current.rotation.y -= 0.0001;
   });
 
   const tilt = props.tilt || 0;
   const tiltb = props.tiltb || 0;
+
+  // Define ring parameters for planets with rings
+  const ringParams: Record<string, { innerRadius: number; outerRadius: number; texture: string }> = {
+    Saturn: { innerRadius: props.size + 0.5, outerRadius: props.size + 7.5, texture: "/textures/saturn_ring.jpg" },
+    Jupiter: { innerRadius: props.size + 0.3, outerRadius: props.size + 4.6, texture: "/textures/jupiter_ring.jpg" },
+    Uranus: { innerRadius: props.size + 0.4, outerRadius: props.size + 4.9, texture: "/textures/uranus_ring.jpg" },
+    Neptune: { innerRadius: props.size + 0.4, outerRadius: props.size + 4.8, texture: "/textures/neptune_ring.jpg" },
+  };
+
   return (
     <>
       <group rotation={[tiltb * (Math.PI / 180), 0, tilt * (Math.PI / 180)]}>
@@ -54,12 +58,12 @@ export function Planet(props: any) {
         )}
 
         {props.name === "Earth" ? <CelestialSphere visible={false} /> : null}
+
         <mesh
           name={props.name}
           visible={props.visible}
           ref={ref}
           scale={1}
-          // rotation={[0, rotationStart, 0]}
           onPointerOver={(e) => {
             e.stopPropagation();
             setHover(true);
@@ -78,15 +82,25 @@ export function Planet(props: any) {
           }}
         >
           <sphereGeometry args={[props.size, 128, 128]} />
-          {/* <meshStandardMaterial map={planetTexture} /> */}
-          {props.texture !== "" ? (
-            <PlanetTexture texture={props.texture} />
+          {props.texture ? (
+            <meshStandardMaterial map={useTexture([props.texture])[0]} />
           ) : (
             <meshStandardMaterial color={props.color} />
           )}
-          {props.light ? <pointLight intensity={3} /> : null}
 
-          {props.name === "Earth" ? <PlanetCamera /> : null}
+          {props.light && <pointLight intensity={3} />}
+
+          {/* Add Rings for planets with rings */}
+          {props.name in ringParams && (
+            <PlanetRings
+              innerRadius={ringParams[props.name].innerRadius}
+              outerRadius={ringParams[props.name].outerRadius}
+              planetSize={props.size}
+              texture={ringParams[props.name].texture}
+            />
+          )}
+
+          {props.name === "Earth" && <PlanetCamera />}
         </mesh>
       </group>
     </>
