5553d3eb29bdc0c5805e8da78afce71fb047efb2
pholmq <pholmq@gmail.com>
2024-10-19 09:38:25 +0200

    Refactoring of PosWriter. It now updates labels and the Positions menu

--- src/components/PosWriter.tsx
+++ src/components/PosWriter.tsx
@@ -1,5 +1,4 @@
 import { Html } from "@react-three/drei";
-import { useControls } from "leva";
 import { useEffect, useRef } from "react";
 import { getRaDecDistance } from "../utils/celestial-functions";
 import { useThree } from "@react-three/fiber";
@@ -12,11 +11,9 @@ export function PosWriter({ hovered, name, symbol = "*", tracked }) {
   const intervalRef = useRef(null);
   const { scene, camera } = useThree();
   const run = useStore((s) => s.run);
-  const posRef = useStore((s) => s.posRef);
-  const runPosWriter = useStore((s) => s.runPosWriter);
-  const { values, updateControls } = useLevaControls();
+  const { updateControls } = useLevaControls();
 
-  function updateLabel() {
+  function updateLabelAndPositions() {
     if (!labelRef.current) return;
 
     const { ra, dec, elongation, distKm, distAU, x, y, z } = getRaDecDistance(
@@ -24,27 +21,9 @@ export function PosWriter({ hovered, name, symbol = "*", tracked }) {
       scene,
       camera
     );
-
-    /* This could be stored in a file and be shown in a different place in the app */
-    const celestialDescriptionsOfTheory = {
-      Earth: "https://book.tychos.space/chapters/11-earths-pvp-orbit",
-      Moon: "The central 'driveshaft'",
-      Sun: "is rather hot!",
-      Halleys: "",
-      Jupiter: "is the largest planet.",
-      Saturn: "",
-      Uranus: "",
-      Neptune: "",
-      Venus: "",
-      Mercury: "",
-      Mars: "was a mystery",
-      Phobos: "",
-      Deimos: "",
-      Eros: "",
-    };
-
-    const description =
-      celestialDescriptionsOfTheory[name] || "Description not available";
+    if (tracked) {
+      updateControls({ [`${name}_RA`]: ra });
+    }
 
     labelRef.current.innerHTML =
       name +
@@ -66,57 +45,17 @@ export function PosWriter({ hovered, name, symbol = "*", tracked }) {
       elongation +
       "\xB0";
   }
-  // const { [name]: on } = useControls("Positions", {
-  //   [name]: {
-  //     value: false,
-  //   },
-  // });
 
-  // useEffect(() => {
-  //   updateLabel();
-  //   const { ra, dec, elongation, distKm, distAU, x, y, z } = getRaDecDistance(
-  //     name,
-  //     scene,
-  //     camera
-  //   );
-  //   // console.log(values, `${name}_RA`);
-  //   if (tracked) {
-  //     updateControls({ [`${name}_RA`]: ra });
-  //   }
-  //   console.log("useffect");
-  // }, [runPosWriter]);
-
-  clearInterval(intervalRef.current);
-  updateLabel();
-  // if (tracked && name && name && camera) {
-  //   console.log(name, scene, camera);
-  //   const { ra, dec, elongation, distKm, distAU, x, y, z } = getRaDecDistance(
-  //     name,
-  //     scene,
-  //     camera
-  //   );
-  //   updateControls({ [`${name}_RA`]: 6 });
-  // }
-
-  if (run) {
-    if (hovered) {
+  useEffect(() => {
+    if (run) {
       intervalRef.current = setInterval(() => {
-        updateLabel();
+        updateLabelAndPositions();
       }, 1000);
     } else {
+      updateLabelAndPositions();
       clearInterval(intervalRef.current);
     }
-
-    if (tracked) {
-      intervalRef.current = setInterval(() => {
-        const { ra, dec, elongation, distKm, distAU, x, y, z } =
-          getRaDecDistance(name, scene, camera);
-        updateControls({ [`${name}_RA`]: ra });
-      }, 1000);
-    } else {
-      clearInterval(intervalRef.current);
-    }
-  }
+  }, [run, hovered]);
 
   return (
     <Html position={[0, 0, 0]} style={{ pointerEvents: "none" }}>
