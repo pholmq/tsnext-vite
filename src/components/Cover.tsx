864a874d2940d76d357c44c3986ca20d6f1c267d
pholmq <pholmq@gmail.com>
2024-10-29 20:26:39 +0100

    Some small fixes

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -3,25 +3,20 @@ import { useFrame } from "@react-three/fiber";
 import { useTexture } from "@react-three/drei";
 import { useStore } from "../store";
 
-export default function Clouds({ size }) {
+export default function Cover({ size, color = "blue" }) {
   const cloudsRef: any = useRef();
   const posRef: any = useStore((state) => state.posRef);
 
   const [cloudsMap] = useTexture(["/textures/2k_earth_clouds.jpg"]);
 
   useFrame(() => {
-    cloudsRef.current.rotation.y -= 0.0001;
+    // cloudsRef.current.rotation.y -= 0.0001;
   });
   return (
     <>
       <mesh ref={cloudsRef} scale={1}>
-        <sphereGeometry args={[size + 0.03, 64, 64]} />
-        <meshPhongMaterial
-          map={cloudsMap}
-          opacity={0.2}
-          depthWrite={true}
-          transparent={true}
-        />
+        <sphereGeometry args={[size + 0.3, 64, 64]} />
+        <meshStandardMaterial color={"blue"} />
       </mesh>
     </>
   );
